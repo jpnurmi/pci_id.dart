17f90561310cc20efa183cfb8f5d2868e6fd9c6e
J-P Nurmi <jpnurmi@gmail.com>
2021-04-15 14:34:08 +0200

    Fix device class parsing

--- tool/src/parser.dart
+++ tool/src/parser.dart
@@ -5,7 +5,7 @@ class PciParser {
     for (final line in lines) {
       final trimmed = line.removeComment();
       if (trimmed.isNotEmpty) {
-        final item = _parseLine(trimmed);
+        final item = parseLine(trimmed);
         _type = item.type;
         _items.add(item);
       }
@@ -13,23 +13,27 @@ class PciParser {
     return _items;
   }
 
-  PciItem _parseLine(String line) {
+  bool isDeviceClass(String line) {
+    return line.startsWith('C ') || _type.index >= PciType.deviceClass.index;
+  }
+
+  PciItem parseLine(String line) {
     final indentation = line.indexOf(RegExp(r'[^\t]'));
     switch (indentation) {
       case 0:
-        if (line.startsWith('C')) {
-          return PciItem.deviceClass(line.trim());
+        if (isDeviceClass(line)) {
+          return PciItem.deviceClass(line.substring(2).trim());
         } else {
           return PciItem.vendor(line.trim());
         }
       case 1:
-        if (_type == PciType.deviceClass) {
+        if (isDeviceClass(line)) {
           return PciItem.subclass(line.trim());
         } else {
           return PciItem.device(line.trim());
         }
       case 2:
-        if (_type == PciType.subclass) {
+        if (isDeviceClass(line)) {
           return PciItem.programmingInterface(line.trim());
         } else {
           return PciItem.subsystem(line.trim());
