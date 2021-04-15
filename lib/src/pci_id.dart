07d13788549a2138e549986caba10e5d35183fe9
J-P Nurmi <jpnurmi@gmail.com>
2021-04-15 14:42:43 +0200

    Add API for device class lookup

--- lib/src/pci_id.dart
+++ lib/src/pci_id.dart
@@ -4,11 +4,21 @@ part 'pci_id.g.dart';
 
 class PciId {
   static Iterable<PciVendor> get allVendors => _vendors.values;
-  static PciVendor? lookupVendor(int id) {
-    return _vendors[id];
-  }
+  static PciVendor? lookupVendor(int id) => _vendors[id];
 
   static PciDevice? lookupDevice(int id, {required int vendorId}) {
     return _devices[vendorId]?[id];
+  }
+
+  static Iterable<PciDeviceClass> get allDeviceClasses {
+    return _device_classes.values;
+  }
+
+  static PciDeviceClass? lookupDeviceClass(int id) {
+    return _device_classes[id];
+  }
+
+  static PciSubclass? lookupSubclass(int id, {required deviceClassId}) {
+    return _subclasses[deviceClassId]?[id];
   }
 }
